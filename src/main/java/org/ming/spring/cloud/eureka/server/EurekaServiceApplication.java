67feb88e1b2d71ca4cd78f3976b6dbeba57c725c
zhouzhec <zz.mark06@gmail.com>
2019-08-28 19:11:36 +0800

    update dependency; remove to unused yml file; adjust eureka config;

--- src/main/java/org/ming/spring/cloud/eureka/server/EurekaServiceApplication.java
+++ src/main/java/org/ming/spring/cloud/eureka/server/EurekaServiceApplication.java
@@ -2,7 +2,6 @@ package org.ming.spring.cloud.eureka.server;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @EnableEurekaServer
@@ -10,7 +9,7 @@ import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 public class EurekaServiceApplication {
 
     public static void main(String[] args) {
-        new SpringApplicationBuilder(EurekaServiceApplication.class).web(true).run(args);
+        SpringApplication.run(EurekaServiceApplication.class, args);
     }
 
 
